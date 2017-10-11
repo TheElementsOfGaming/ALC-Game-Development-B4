fa9eab3d293aefdbca08fd53ff762c11170ee267
Kyler Draper <kythegamedeveloper@gmail.com>
2017-10-11 13:07:51 -0600

    Added Ghost and playerMove script

--- Haunted House/Assets/Scripts/PlayerMove.cs
+++ Haunted House/Assets/Scripts/PlayerMove.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
+	public float moveSpeed; 
+	public float turnSpeed;
+	public float jumpHeight;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+		var j = Input.GetAxis("Jump") * Time.deltaTime * jumpHeight;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
 	}
 }
