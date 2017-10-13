2444235b189b9f902a137c9c0581d6c4cc08c12e
Kyler Draper <kythegamedeveloper@gmail.com>
2017-10-13 13:12:29 -0600

    Added Prefab folder

---
+++ Haunted House/Assets/Scripts/GhostAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : MonoBehaviour {
+
+	public float moveSpeed;
+
+	public Transform target; 
+
+	public int damage; 
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime)
+		}
+	}		
+}
