5772bdb7321434754e3e8f9352412ce63f8d75dc
Kyler Draper <kythegamedeveloper@gmail.com>
2017-10-17 13:02:50 -0600

    Fixed Ghost AI, and added Flashlight

--- Haunted House/Assets/Scripts/GhostMove.cs
+++ Haunted House/Assets/Scripts/GhostMove.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ghost : MonoBehaviour {
+public class GhostMove : MonoBehaviour {
 
 	public float moveSpeed;
-
 	public Transform target; 
-
 	public int damage; 
+
+
+
 
 	void OnTriggerStay(Collider other)
 	{
+
 		if(other.gameObject.name == "Player"){
 			transform.LookAt(target);
-			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime)
+			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
 		}
+		
 	}		
 }
