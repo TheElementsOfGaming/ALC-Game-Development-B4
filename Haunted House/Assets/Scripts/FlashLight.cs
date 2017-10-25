ce4719194d12319773478cccd0f1f49d50c91ddc
Kyler Draper <kythegamedeveloper@gmail.com>
2017-10-25 13:10:33 -0600

    WIP Floor layout and added on/off Flashlight script

---
+++ Haunted House/Assets/Scripts/FlashLight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class FlashLight : MonoBehaviour {
+
+public bool lightOn; 
+
+Light light; 
+
+//use this for initialization 
+void Start() {
+	light = GetComponent<Light> ();
+	lightOn = true;
+	light.enabled= true; 
+}
+
+//update is called once per frame 
+void Update (){
+	//Toggle light on/off when L key is pressed.
+	if (Input.GetKeyUp (KeyCode.L) && lightOn){
+		lightOn = false;
+		light.enabled = false;
+	}
+	else if (Input.GetKeyUp (KeyCode.L) && !lightOn){
+		lightOn = true; 
+		light.enabled = true;
+	
+		}
+	}
+}
