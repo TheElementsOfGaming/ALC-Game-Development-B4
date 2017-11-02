ae33f4e88b17d45b1ed843a472e2acd0ce65c0ba
Kyler Draper <kythegamedeveloper@gmail.com>
2017-11-02 13:05:20 -0600

    Added GhostStun script

---
+++ Haunted House/Assets/Scripts/GhostStun.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostStun : MonoBehaviour {
+
+	//bool lightCheck;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		
+		if(other.gameObject.name == "Ghost"){
+			print("Ghost is stunned!");		
+		}
+	}
+}
