58683e53de2a4509d4259cfd70a876e72f807db7
Kyler Draper <kythegamedeveloper@gmail.com>
2017-11-27 12:26:19 -0700

    Created Battery/BatteryScript, and EvilVillagerMan

---
+++ Haunted House/Assets/Scripts/Battery_power.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery_power : MonoBehaviour {
+
+public int power = 4;
+
+public GameObject flashlight;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Player"){
+
+		}
+	}
+}
